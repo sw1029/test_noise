116f76cfb74dd1bb8cf229a98d4db13f941df109
sw1029 <yunsw62@gmail.com>
2025-08-06 10:18:54 +0900

    noise class의 base

---
+++ test_noise/noise_generator/base.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Noise(ABC):
+    """
+    모든 노이즈에 대한 기본 클래스입니다. 추후 기능 확장시 해당 클래스를 상속하여 확장합니다.
+
+    factor, threshold 등의 파라미터를 추가 시 init을 정의 후 입력받게 하여 구현 가능합니다.
+    """
+
+    @abstractmethod
+    def add_noise(self) -> np.ndarray:
+        """
+        입력된 이미지에 노이즈를 적용하는 부분입니다.
+        init에서 입력받은 이미지에 noise를 추가한 후 반환합니다. 
+        """
+        pass
