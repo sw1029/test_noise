62fd0d04304ec49aee6d65630d09abf200709eaa
sw1029 <yunsw62@gmail.com>
2025-08-22 06:16:04 +0900

    noise_generator init에서 atmospheric, terrain, sun angle noise 노출

--- test_noise/noise_generator/__init__.py
+++ test_noise/noise_generator/__init__.py
@@ -3,9 +3,15 @@ from .poisson_noise import PoissonNoise
 from .salt_pepper_noise import SaltPepperNoise
 from .vignetting_noise import VignettingNoise
 from .missing_line_noise import MissingLineNoise
+from .atmospheric_noise import AtmosphericNoise
+from .terrain_noise import TerrainNoise
+from .sun_angle_noise import SunAngleNoise
 
 gaussianNoise = GaussianNoise.add_noise
 poissonNoise = PoissonNoise.add_noise
 saltPepperNoise = SaltPepperNoise.add_noise
 vignettingNoise = VignettingNoise.add_noise
 missingLineNoise = MissingLineNoise.add_noise
+atmosphericNoise = AtmosphericNoise.add_noise
+terrainNoise = TerrainNoise.add_noise
+sunAngleNoise = SunAngleNoise.add_noise
