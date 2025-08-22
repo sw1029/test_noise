8593e67a4af57b13fff623df4d94f9ef6725006c
sw1029 <yunsw62@gmail.com>
2025-08-22 16:30:50 +0900

    골자  추가

--- test_noise/noise_eval/param_finder.py
+++ test_noise/noise_eval/param_finder.py
@@ -2,3 +2,11 @@
 균일한 노이즈 평가를 위한 파라미터 탐색기
 '''
 
+from noise_generator import *
+from metric import *
+
+def find_param(img, target_value, noise_type = 'gaussian',value_type = "rmse"):
+    '''
+    이진 탐색을 통하여 선택한 noise의 target_value에 도달하는 파라미터를 찾는다.
+    '''
+    pass
