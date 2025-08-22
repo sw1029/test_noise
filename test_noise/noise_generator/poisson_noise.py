f93e0aba3153c6cc633790741e75edff618ee08b
sw1029 <yunsw62@gmail.com>
2025-08-22 16:34:38 +0900

    poisson noise에 대하여 factor 인자 추가

--- test_noise/noise_generator/poisson_noise.py
+++ test_noise/noise_generator/poisson_noise.py
@@ -3,8 +3,10 @@ import numpy as np
 
 class PoissonNoise(Noise):
     @staticmethod
-    def add_noise(src) -> np.ndarray:
+    def add_noise(src, factor = 0.1) -> np.ndarray:
         noisy_image = np.copy(src)
         for i in range(src.shape[2]):  # 채널별로 독립적으로 적용
             noisy_image[:, :, i] = np.random.poisson(src[:, :, i]).astype(np.uint8)
+        noisy_image = src * (1 - factor) + noisy_image * factor
+        noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
         return noisy_image
