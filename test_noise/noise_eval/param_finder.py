3e80a6c8a14c4ea3f800dad026ec2a62b6054023
sw1029 <yunsw62@gmail.com>
2025-08-22 16:56:19 +0900

    param finder 구현

--- test_noise/noise_eval/param_finder.py
+++ test_noise/noise_eval/param_finder.py
@@ -4,9 +4,61 @@
 
 from noise_generator import *
 from metric import *
+import numpy as np
 
-def find_param(img, target_value, noise_type = 'gaussian',value_type = "rmse"):
+NOISE = {
+    'gaussian': gaussianNoise,
+    'salt_pepper': saltPepperNoise,
+    'vignetting': vignettingNoise,
+    'missingLine': missingLineNoise,
+    'striping': stripingNoise,
+    'sunAngle': sunAngleNoise,
+    'terrain': terrainNoise,
+    'atmospheric': atmosphericNoise,
+    'poisson': poissonNoise
+}
+
+METRIC = {
+    'psnr': {'func': psnr, 'positive': False},
+    'rmse': {'func': rsme, 'positive': True},
+    'mae': {'func': mae, 'positive': True},
+    'ssim': {'func': ssim, 'positive': False}
+}
+
+PARAMS = {
+    'gaussian': ('var', 0, 10000),
+    'salt_pepper': ('amount', 0.0, 1.0),
+    'vignetting': ('strength', 0.0, 1.0),
+    'missing_line': ('num_threshold', 1, 100), # 다른 파라미터는 기본값 사용
+    'striping': ('noise_strength', 0, 50),
+    'sun_angle': ('intensity', 0.0, 1.0),
+    'terrain': ('factor', 0.0, 1.0),
+    'atmospheric': ('factor', 0.0, 1.0),
+    'poisson': ('factor', 0.0, 1.0)
+}
+
+def find_param(img, target, 
+               noise_type = 'gaussian',value_type = "rmse", 
+               iter=100 , tol=0.01):
     '''
     이진 탐색을 통하여 선택한 noise의 target_value에 도달하는 파라미터를 찾는다.
     '''
-    pass
+    noise = NOISE[noise_type]
+    metric = METRIC[value_type]
+    param_name, min, max = PARAMS[noise_type]
+
+    positive = metric['positive']
+
+    for _ in range(iter):
+        mid = (min + max) / 2
+        noisy_img = noise(img, **{param_name: mid})
+        value = metric(img, noisy_img)
+
+        if abs(value - target) < tol:
+            return mid
+
+        if (positive and value < target) or (not positive and value > target):
+            min = mid
+        else:
+            max = mid
+    return (min + max) / 2
