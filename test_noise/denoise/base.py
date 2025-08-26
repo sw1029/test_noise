60e8085d71970b8a2b97bae20f489c86c3148645
sw1029 <yunsw62@gmail.com>
2025-08-26 16:02:20 +0900

    denoise 기본 추상 class

---
+++ test_noise/denoise/base.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Denoise(ABC):
+    """
+    기본 클래스입니다. 추후 기능 확장시 해당 클래스를 상속하여 확장합니다.
+
+    factor, threshold 등의 파라미터를 추가 시 init을 정의 후 입력받게 하여 구현 가능합니다.
+    """
+    @staticmethod
+    @abstractmethod
+    def denoise(src: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        입력된 이미지에 노이즈를 보정하는 부분입니다.
+        init에서 입력받은 이미지에 noise를 보정한 후 반환합니다. 
+        """
+        pass
