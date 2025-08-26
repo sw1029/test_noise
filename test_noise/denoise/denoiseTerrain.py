29cc09cdedaa81b1211b1d604a62db9a35d5a480
sw1029 <yunsw62@gmail.com>
2025-08-27 03:33:22 +0900

    radiance에서 factor 적용하는 코드 revert

--- test_noise/denoise/denoiseTerrain.py
+++ test_noise/denoise/denoiseTerrain.py
@@ -6,7 +6,7 @@ import os
 
 class DenoiseTerrain(Denoise):
     @staticmethod
-    def denoise(src, factor = 0.3,
+    def denoise(src, factor = 0.7,
                   sun_angle=30, 
                   DEM=None, pixel_size=1.0,
                   slope=30, 
@@ -41,9 +41,9 @@ class DenoiseTerrain(Denoise):
             _slope = np.rad2deg(_slope) 
         else : _slope = slope     
 
-        #radiance_B = np.clip(DN2radiance(src[:,:,0], gain_B, offset_B), 0, None)
-        #radiance_G = np.clip(DN2radiance(src[:,:,1], gain_G, offset_G), 0, None)
-        #radiance_R = np.clip(DN2radiance(src[:,:,2], gain_R, offset_R), 0, None)
+        radiance_B = np.clip(DN2radiance(src[:,:,0], gain_B, offset_B), 0, None)
+        radiance_G = np.clip(DN2radiance(src[:,:,1], gain_G, offset_G), 0, None)
+        radiance_R = np.clip(DN2radiance(src[:,:,2], gain_R, offset_R), 0, None)
         
         radiance_B = DN2radiance(src[:,:,0], gain_B, offset_B)
         radiance_G = DN2radiance(src[:,:,1], gain_G, offset_G)
@@ -53,20 +53,15 @@ class DenoiseTerrain(Denoise):
         terrain_denoise_image[:, :, 1] = Minnaert(radiance_G, sun_angle, _slope, Minnaert_constant_G)
         terrain_denoise_image[:, :, 2] = Minnaert(radiance_R, sun_angle, _slope, Minnaert_constant_R)
 
-        eps = 1e-9
-        denoised_radiance_B = (radiance_B - terrain_denoise_image[:, :, 0] * factor) / (1 - factor + eps)
-        denoised_radiance_G = (radiance_G - terrain_denoise_image[:, :, 1] * factor) / (1 - factor + eps)
-        denoised_radiance_R = (radiance_R - terrain_denoise_image[:, :, 2] * factor) / (1 - factor + eps)
-
-        terrain_denoise_image[:, :, 0] = radiance2DN(denoised_radiance_B, gain_B, offset_B)
-        terrain_denoise_image[:, :, 1] = radiance2DN(denoised_radiance_G, gain_G, offset_G)
-        terrain_denoise_image[:, :, 2] = radiance2DN(denoised_radiance_R, gain_R, offset_R)
+        terrain_denoise_image[:, :, 0] = radiance2DN(terrain_denoise_image[:, :, 0], gain_B, offset_B)
+        terrain_denoise_image[:, :, 1] = radiance2DN(terrain_denoise_image[:, :, 1], gain_G, offset_G)
+        terrain_denoise_image[:, :, 2] = radiance2DN(terrain_denoise_image[:, :, 2], gain_R, offset_R)
         
         if channels == 4:
             radiance_NIR = DN2radiance(src[:, :, 3], gain_NIR, offset_NIR)
             terrain_denoise_image[:, :, 3] = Minnaert(radiance_NIR, sun_angle, _slope, Minnaert_constant_NIR)
-            denoised_radiance_NIR = (radiance_NIR - terrain_denoise_image[:, :, 3] * factor) / (1 - factor + eps)
-            terrain_denoise_image[:, :, 3] = radiance2DN(denoised_radiance_NIR, gain_NIR, offset_NIR)
+            terrain_denoise_image[:, :, 3] = radiance2DN(terrain_denoise_image[:, :, 3], gain_NIR, offset_NIR)
        
+        terrain_denoise_image = src + factor * (terrain_denoise_image - src)
         terrain_denoise_image = np.clip(terrain_denoise_image, 0, 255).astype(np.uint8)
         return terrain_denoise_image
