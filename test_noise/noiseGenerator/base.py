4e89627218f022687e886c0ac2d70b7d4e826a0a
sw1029 <yunsw62@gmail.com>
2025-08-29 18:29:11 +0900

    주석 어조 수정

--- test_noise/noiseGenerator/base.py
+++ test_noise/noiseGenerator/base.py
@@ -3,15 +3,15 @@ import numpy as np
 
 class NoiseBase(ABC):
     """
-    모든 노이즈에 대한 기본 클래스입니다. 추후 기능 확장시 해당 클래스를 상속하여 확장합니다.
+    모든 노이즈에 대한 기본 클래스. 추후 기능 확장시 해당 클래스를 상속하여 확장.
 
-    factor, threshold 등의 파라미터를 추가 시 init을 정의 후 입력받게 하여 구현 가능합니다.
+    factor, threshold 등의 파라미터를 추가 시 init을 정의 후 입력받게 하여 구현 가능.
     """
     @staticmethod
     @abstractmethod
     def add_noise(src: np.ndarray, **kwargs) -> np.ndarray:
         """
         입력된 이미지에 노이즈를 적용하는 부분입니다.
-        init에서 입력받은 이미지에 noise를 추가한 후 반환합니다. 
+        init에서 입력받은 이미지에 noise를 추가한 후 반환. 
         """
         pass
