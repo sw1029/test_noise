7f23d5c8e0898fc5bbd998e46a9517359a8b24fe
sw1029 <yunsw62@gmail.com>
2025-08-29 19:39:26 +0900

    DEM 적용 시 Minnaert 보정을 통해 보정하도록 수정.

--- test_noise/denoise/denoiseTerrain.py
+++ test_noise/denoise/denoiseTerrain.py
@@ -6,10 +6,9 @@ import os
 
 class DenoiseTerrain(Denoise):
     @staticmethod
-    def denoise(src, factor = 0.3,
-                  sun_angle=30, 
-                  DEM=None, pixel_size=1.0,
-                  slope=30, max_slope = 60,
+    def denoise(src, DEM=None, pixel_size=1.0,
+                  sun_angle=30, sun_azimuth=225, sun_elevation=45, 
+                  factor=0.1, slope=30, max_slope=45,
                   Minnaert_constant_NIR=0.6,
                   Minnaert_constant_R=0.5,
                   Minnaert_constant_G=0.4,
@@ -17,7 +16,7 @@ class DenoiseTerrain(Denoise):
                   yaml_name="KOMPSAT.yaml") -> np.ndarray:
         current_dir = os.path.dirname(os.path.abspath(__file__))
         config_path = os.path.join(current_dir, '..', 'config', yaml_name)
-        
+        _sun_angle = sun_angle
         with open(config_path, 'r') as f:
             config = yaml.safe_load(f)
         
@@ -36,20 +35,7 @@ class DenoiseTerrain(Denoise):
         terrain_denoise_image = src.copy()
 
         if DEM is not None: # DEM 이용 단순 보정
-            px, py = np.gradient(DEM)
-            _slope = np.arctan(np.sqrt(px**2 + py**2))
-            _slope = np.rad2deg(_slope)
-
-            np.clip(_slope, 0, max_slope, out=_slope)
-            _slope = _slope / max_slope
-            
-            mask = 1.0 - _slope
-            mask = mask[:, :, np.newaxis]
-            
-            div = (1 - factor) + (mask * factor)
-            div[div == 0] = 1e-9
-            img = src / div
-            return img
+            _sun_angle, _slope = angle(DEM, sun_azimuth, sun_elevation)
         else : _slope = slope    
         
         # 해당 radiance 값에 음수 clipping 적용 시 단색 이미지가 반환되는 오류가 발생.
@@ -57,9 +43,9 @@ class DenoiseTerrain(Denoise):
         radiance_G = DN2radiance(src[:,:,1], gain_G, offset_G)
         radiance_R = DN2radiance(src[:,:,2], gain_R, offset_R)
 
-        terrain_denoise_image[:, :, 0] = Minnaert(radiance_B, sun_angle, _slope, Minnaert_constant_B)
-        terrain_denoise_image[:, :, 1] = Minnaert(radiance_G, sun_angle, _slope, Minnaert_constant_G)
-        terrain_denoise_image[:, :, 2] = Minnaert(radiance_R, sun_angle, _slope, Minnaert_constant_R)
+        terrain_denoise_image[:, :, 0] = Minnaert(radiance_B, _sun_angle, _slope, Minnaert_constant_B)
+        terrain_denoise_image[:, :, 1] = Minnaert(radiance_G, _sun_angle, _slope, Minnaert_constant_G)
+        terrain_denoise_image[:, :, 2] = Minnaert(radiance_R, _sun_angle, _slope, Minnaert_constant_R)
 
         terrain_denoise_image[:, :, 0] = radiance2DN(terrain_denoise_image[:, :, 0], gain_B, offset_B)
         terrain_denoise_image[:, :, 1] = radiance2DN(terrain_denoise_image[:, :, 1], gain_G, offset_G)
@@ -67,7 +53,7 @@ class DenoiseTerrain(Denoise):
         
         if channels == 4:
             radiance_NIR = DN2radiance(src[:, :, 3], gain_NIR, offset_NIR)
-            terrain_denoise_image[:, :, 3] = Minnaert(radiance_NIR, sun_angle, _slope, Minnaert_constant_NIR)
+            terrain_denoise_image[:, :, 3] = Minnaert(radiance_NIR, _sun_angle, _slope, Minnaert_constant_NIR)
             terrain_denoise_image[:, :, 3] = radiance2DN(terrain_denoise_image[:, :, 3], gain_NIR, offset_NIR)
        
         terrain_denoise_image = np.nan_to_num(terrain_denoise_image)
